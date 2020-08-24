b7fa08718cb04dbd2dcf1cef15ea291b977c19db
OpiWrites <70041411+OpiWrites@users.noreply.github.com>
2020-08-24 18:24:54 -0400

    Update SpyPartyRankedPrototypev1.py

--- SpyPartyRanked/SpyPartyRankedPrototypev1.py
+++ SpyPartyRanked/SpyPartyRankedPrototypev1.py
@@ -1,25 +1,31 @@
+import os
+import gzip
 from ReplayParser import * ###Import parse function
 def readLog(logName):
-    print("Entered method")
+    
     replayList = []
     rankedIsOn = 0
-    file = open(logName, 'r')
-    logLines = file.readlines()
-    for line in logLines:
-        if line.count("you choose") == 1: ##Turns on ranked when key phrase is found
-            rankedIsOn = 1
-            print("Ranked is on")
-        if rankedIsOn == 1 and line.count("LobbyClient starting to lobby from IN_MATCH") == 1: ##Turns off ranked when match ends
-            rankedIsOn = 0
-            print("Ranked is off")
-        if rankedIsOn == 1 and line.count("Writing replay") == 1: ##Finds and saves replay paths while ranked is on
-            replayFind = line.split(": ") ##Make this regex later
-            pathString = replayFind[2]
-            pathString = pathString.rstrip()
-            pathString = pathString.strip('\"')
-            replayList.append(pathString) 
-            print("Found replay, saving path")
-    return replayList
+    try:
+        with gzip.open(logName, 'rt') as file:
+            logLines = file.readlines()
+            for line in logLines:
+                if line.count("Ranked mode on") and line.count("LobbyClient sending chat message") == 1: ##Turns on ranked when key phrase is found in local chat logs
+                    rankedIsOn = 1
+                    print("Ranked is on")
+                elif rankedIsOn == 1 and line.count("LobbyClient starting to lobby from IN_MATCH") == 1: ##Turns off ranked when match ends
+                    rankedIsOn = 0
+                    print("Ranked is off")
+                elif rankedIsOn == 1 and line.count("Writing replay") == 1: ##Finds and saves replay paths while ranked is on
+                    replayFind = line.split(": ")
+                    pathString = replayFind[2]
+                    pathString = pathString.rstrip()
+                    pathString = pathString.strip('\"')
+                    replayList.append(pathString) 
+                    print("Found replay, saving path")
+            file.close
+            return replayList
+    except:
+        print("SpyParty is running!")
 
 def getResult(replayPath):
     hummus = ReplayParser()
@@ -30,6 +36,17 @@ def getResult(replayPath):
     venue = replay.venue
     print(venue, spyName, sniperName, result)
 
-replayList = readLog("C:/Users/Aidan/AppData/Local/SpyParty/logs/SpyPartyTestFile.log")
-for replayPath in replayList:
-    getResult(replayPath)
+def findLogPath():
+    Dir = os.getcwd()
+    parenDir = os.path.dirname(Dir)
+    return(parenDir + "\\logs")
+
+def findLog(logPath):
+    lastEdited = 0
+    logFile = ""
+    for file in os.scandir(logPath):
+        if os.path.getctime(file) > lastEdited:
+            lastEdited = os.path.getctime(file)
+            logFile = os.path.join(logPath, file)
+    return logFile    
+
